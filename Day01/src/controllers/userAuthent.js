2f7145e369f59613587d3c3ca47c9b900a2550a3
pandujadhav <2022bit054@sggs.ac.in>
2025-09-08 04:08:21 +0530

    implemented login status

--- Day01/src/controllers/userAuthent.js
+++ Day01/src/controllers/userAuthent.js
@@ -22,3 +22,29 @@ const register = async (req,res)=>{
         res.status(400).send("Error: "+err);
     }
 }
+
+const login = async (req,res)=>{
+    try{
+        const {emailId, password} = req.body;
+        if(!emailId)
+            throw new Error("Invalid Credentials");
+        if(!password)
+            throw new Error("Invalid Credentials");
+
+        const user = await user.findOne({emailId});
+
+        const match = bcrypt.compare(password,user.password);
+
+        if(!match)
+            throw new Error("invalid credentials");
+
+        const token = jwt.sign({_id:user._id , emailId:emailId},process.env.JWT_KEY,{expiresIn: 60*60});
+        res.cookie('token',token,{maxAge: 60*60*1000});
+
+        res.status(200).send("logged In sucessfully");
+        
+    }
+    catch(err){
+        res.status(401).send("Error:"+err);
+    }
+}
