8001fba7121359387b97e1d37d7982ec01f2aff8
pandujadhav <2022bit054@sggs.ac.in>
2025-09-09 09:25:15 +0530

    .env files clear the bugs from main folder

--- Day01/src/index.js
+++ Day01/src/index.js
@@ -1,17 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 require('dotenv').config();
 const main = require('./config/db');
 const cookieParser = require('cookie-parser');
+const authRouter = require("./routes/userAuth")
+
 
 
 app.use(express.json());  // parser 
 app.use(cookieParser())
+app.use('/user',authRouter);
+
+
 main()
 .then(async ()=>{
 app.listen(process.env.PORT, ()=>{
     console.log("server listening  at a port "+process.env.PORT);
    })
+  
 })
 
-.catch(err=> console.log("error Occured:"+ process.env.PORT));
+.catch(err=> console.log("error Occured:"+ err.message)); 
